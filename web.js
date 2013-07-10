2738bade97ad20df817d7034bc6370976e413ab2
Amy Robert <uncamy@gmail.com>
2013-07-10 02:46:23 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html));
+app.get('/', function(request, response) { 
+  response.send(fs.readFileSync(index.html,encoding));
 });
 
 var port = process.env.PORT || 5000;
