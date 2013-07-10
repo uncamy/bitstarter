8ced53c2e36488d63f360c7366c489cb3121f7ce
Amy Robert <uncamy@gmail.com>
2013-07-10 17:13:49 +0000

    attempting to add url capabilites

--- grader.js
+++ grader.js
@@ -37,7 +37,7 @@ var assertFileExists = function(infile) {
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerio.load(result);
 };
 
 var loadChecks = function(checksfile) {
@@ -46,14 +46,16 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for (var ii in checks) {
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
-    }
-    return out;
-};
+    if(program.url){
+	rest.get(program.url).on('complete', function(result){
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for (var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    return out;
+}};
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -65,7 +67,8 @@ if(require.main == module) {
     program
        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists),CHECKSFILE_DEFAULT)
        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+       .option('-u, --url <html_file>', 'url to index.html')
+	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
