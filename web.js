cd57ea72d8094da7216fc7cea21681d7e6b43ba5
Amy Robert <uncamy@gmail.com>
2013-07-10 03:03:57 +0000

    Updated web.js to read index.html- correcting err

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) { 
-  response.send(fs.readFileSync(index.html,encoding));
+var buf = fs.readFileSync('index.html', 'utf-8');
+var string =buf.toString();
+app.get('/', function(request, response)  {
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
